75a6965d8df9793d5fb0a33d81387c3f652f1705
Akio co <fifikoridek@gmail.com>
2026-06-22 17:04:48 +0100

    Delete raqip-main directory

--- raqip-main/android/app/src/main/kotlin/com/example/myapp/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-// MainActivity تستخدم FlutterActivity الافتراضية
-// لعرض واجهة Flutter على Android.
-class MainActivity : FlutterActivity()
